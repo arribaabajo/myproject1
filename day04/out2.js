6dc9a965707fbee52b11855de3c6713781b3b526
donwooyi <124110726+arribaabajo@users.noreply.github.com>
2023-02-02 17:50:01 +0900

    Add files via upload

---
+++ day04/out2.js
@@ -0,0 +1,12 @@
+function 여행하다(){
+console.log("1. 계획을 수립한다.")
+console.log("2. 예약한다.")
+console.log("3. 이동한다.")
+console.log("4. 식사를 한다.")
+console.log("5. 돌아온다.")
+}
+function 쉬다(){
+console.log("1. 샤워를 한다")
+console.log("2. 식사를 털자.")
+console.log("잔다.")
+}
